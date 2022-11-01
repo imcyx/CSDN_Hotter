1fc8c975ac73f26e0f8385930e3e52aad02f7f06
ChenYuxi <im.cyx@foxmail.com>
2022-11-01 14:27:50 +0800

    test0

--- scripts/proxypool_intsall.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/Python3WebSpider/ProxyPool.git;
-cd ProxyPool;
-wget https://download.redis.io/releases/redis-7.0.5.tar.gz;
-tar -zxvf redis-7.0.5.tar.gz;
-cd redis-7.0.5;
-make MALLOC=libc;
-mkdir lib && cp -rf redis-5.0.14/src ./lib;
-cd src && make install;
-redis-server;
